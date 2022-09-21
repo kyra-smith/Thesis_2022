4df9323927a54142255122854d871decff06fbe6
kyra-smith <73694089+kyra-smith@users.noreply.github.com>
2022-09-21 17:35:50 +0200

    Update 9_ModelChecks.R

--- 9_ModelChecks.R
+++ 9_ModelChecks.R
@@ -42,14 +42,88 @@ mod_list <- c("LUIAbund", "LUIRich","MeanAnomalyModelAbund", "MeanAnomalyModelRi
 mod_list1 <- c("LUIAbund", "LUIRich")
 mod_list2 <- c("MeanAnomalyModelAbund", "MeanAnomalyModelRich", "MaxAnomalyModelAbund", "MaxAnomalyModelRich")
 
-# load dataset
+# load datasets
+model_data_sr <- readRDS(file = paste0(LUIDir,"model_data_sr.rds"))
+model_data_ab <- readRDS(file = paste0(LUIDir,"model_data_ab.rds"))
+
 predictsSites <- readRDS(paste0(predictsDataDir,"PREDICTSSitesClimate_Data.rds"))
 predictsSites <- predictsSites[!is.na(predictsSites$StdTmeanAnomalyRS), ]
 
 #x <- mod_list[1]
 
+# for the Order*LUI models
+for(x in mod_list1){
+  
+  # only do this one if not SR model
+  # grepl() searches for matches to "Abun" in "x", the list of models
+  if(grepl("Abun", x) ==1) {
+    
+    
+    ## 1. Checking the fitted vs residuals relationship
+    p1 <- plot(get(x)$model)
+  }
+  
+  ## 2. Normality of Residuals
+  pdf(NULL)
+  dev.control(displaylist="enable")
+  qqnorm(resid(get(x)$model), main = "")
+  qqline(resid(get(x)$model))
+  p2 <- recordPlot()
+  invisible(dev.off())
+  
+  
+  if(grepl("Abun", x) == 1) {
+    
+    
+    predData <- model_data_ab[!is.na(model_data_ab$LogAbund), ]
+    
+    
+    # 3. plot of observed vs fitted values
+    pdf(NULL)
+    dev.control(displaylist="enable")
+    plot(predData$LogAbund,fitted(get(x)$model), 
+         xlab = "Observed values", ylab = "Fitted values") 
+    abline(a = 0, b = 1, col = "red", lwd = 2)
+    p3 <- recordPlot()
+    invisible(dev.off())
+    
+    
+    cowplot::plot_grid(p1,p2,p3,
+                       labels = c("A.", "B.", "C."))
+    
+    ggsave(file = paste0(outdir, x, "_model_checks.pdf"), height = 10, width = 10)
+    
+    rm(p1, p2, p3)
+    
+    
+  }else{
+    
+    
+    # 4. plot of observed vs fitted values
+    
+    pdf(NULL)
+    dev.control(displaylist="enable")
+    plot(model_data_sr$Species_richness,fitted(get(x)$model), 
+         xlab = "Observed values", ylab = "Fitted values") 
+    abline(a = 0, b = 1, col = "red", lwd = 2)
+    p3 <- recordPlot()
+    invisible(dev.off())
+    
+    cowplot::plot_grid(p2,p3,
+                       labels = c("A.", "B."))
+    
+    ggsave(file = paste0(outdir, x, "_model_checks.pdf")) 
+    
+    rm(p2, p3)
+    
+  }
+  
+}
+
+# for the Order*LUI*Climate Models
+
 # loop through models and generate plots for checking assumptions etc
-for(x in mod_list){
+for(x in mod_list2){
   
   # only do this one if not SR model
   # grepl() searches for matches to "Abun" in "x", the list of models
@@ -78,7 +152,7 @@ for(x in mod_list){
     # 3. plot of observed vs fitted values
     pdf(NULL)
     dev.control(displaylist="enable")
-    plot(predData$LogAbund,fitted(get(x)), 
+    plot(predData$LogAbund,fitted(get(x)$model), 
          xlab = "Observed values", ylab = "Fitted values") 
     abline(a = 0, b = 1, col = "red", lwd = 2)
     p3 <- recordPlot()
@@ -100,14 +174,14 @@ for(x in mod_list){
     
     pdf(NULL)
     dev.control(displaylist="enable")
-    plot(predictsSites$Species_richness,fitted(get(x)), 
+    plot(predictsSites$Species_richness,fitted(get(x)$model), 
          xlab = "Observed values", ylab = "Fitted values") 
     abline(a = 0, b = 1, col = "red", lwd = 2)
     p3 <- recordPlot()
     invisible(dev.off())
     
     cowplot::plot_grid(p2,p3,
-                       labels = c("A.", "B."))#
+                       labels = c("A.", "B."))
     
     ggsave(file = paste0(outdir, x, "_model_checks.pdf")) 
     
